884ee95b674db9522a3bd4bc903a596257a2e414
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-12-29 21:58:23 +0100

    Moved to Python Crash Course Directory

---
+++ 01_Python_Crash_Course/01_Hello_Python/hello.py
@@ -0,0 +1,15 @@
+#!/bin/bash/python3
+
+# Printing Basic Output
+print("Hello world")
+
+name = "Samuel"
+print("Hello", name)
+
+# Basic Arithmetic Operations
+house_rooms = 4
+chairs_per_room = 3
+total_chairs = house_rooms * chairs_per_room
+print("Toal chairs in the house are", total_chairs, "chairs")
+
+ 
