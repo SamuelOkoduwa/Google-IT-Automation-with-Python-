1f3cb80e5d0c8fff3512dc9e9d134b92ea480569
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-01-04 13:25:38 +0100

    Basic python braching with if, elif and else statements

---
+++ 01_Python_Crash_Course/02_Basic_Python_Sytax/4.conditions.py
@@ -0,0 +1,169 @@
+#!/usr/bin/python3
+
+""'''Examples'''""
+
+# The greater than > operator checks if the left string has a higher 
+# Unicode value than the right string. If true, the Python interpreter
+# returns a True result. Since W has a Unicode value of 87, and you can 
+# easily calculate that F has a Unicode value of 70, this comparison is
+# the same as 87 > 70. As this is true, Python will return a True 
+# result.
+print("Wednesday" > "Friday")
+True
+ 
+ 
+# The less than < operator checks if the left string has a lower 
+# Unicode value than the right string. If you reference the Unicode 
+# chart above, you can see that all lowercase letters have higher 
+# Unicode values than uppercase letters. We can see that B has a 
+# Unicode value of 66 and b has a Unicode value of 98. This 
+# comparison is the same as 66 < 98, which is true. So, Python will 
+# return a True result.
+print("Brown" < "brown")
+True
+
+
+# If the strings have the same first few letters, the comparison will 
+# cycle through each letter of each string, from left to right until it 
+# finds two letters that have different Unicode values. In this example, 
+# both strings share the initial substring "sun", but then have 
+# different letters with different Unicode values in the fourth place 
+# in each string. So, the fourth letters 'b' and 't' of the two
+# strings are used for the comparison. Since 'b' does not have a higher
+# Unicode value than 't', the comparison returns a False result.
+print("sunbathe" > "suntan")
+False
+
+
+# If two identical strings are compared using the less than < comparison
+# operator, this will produce a False result because they are equal.
+print("Lima" < "Lima")
+False
+
+
+# This last example illustrates the result of trying to compare two
+# items of different data types using the less than < operator. The 
+# greater than > and less than operators < cannot be used to compare
+# two different data types. 
+print("Five" < 6)
+'''
+Error on line 1:
+    print("Five" < 6)
+TypeError: '<' not supported between instances of 'str' and 'int'
+'''
+
+
+
+"""Branching with if stattements
+    This is the ability of a code to alter it's execution sequence
+"""
+
+# Practice Quiz condition
+# 1)
+'''
+    What's the value of this Python expression: (2**2) == 4?
+    (a) 4 (b) 2**2 (c) True (d) False
+    The solution below
+'''
+# The answer => True
+
+
+
+# 2)
+'''
+    Question 2
+Complete the script by filling in the missing parts. The function receives a name, then returns a greeting based on whether or not that name is "Taylor".
+
+def greeting(name):
+  if ___ == "Taylor":
+    return "Welcome back Taylor!"
+  ___:
+    return "Hello there, " + name
+
+print(greeting("Taylor"))
+print(greeting("John"))
+
+The solution below
+'''
+def greeting(name):
+  if name == "Taylor":
+    return "Welcome back Taylor!"
+  else:
+    return "Hello there, " + name
+
+print(greeting("Taylor"))
+print(greeting("John"))
+
+
+# 3)
+'''
+    Question 3
+What’s the output of this code if number equals 10?
+if number > 11: 
+  print(0)
+elif number != 10:
+  print(1)
+elif number >= 20 or number < 12:
+  print(2)
+else:
+  print(3)
+
+The solution below
+'''
+# The answer => 3
+# 4)
+'''
+    Question 4
+Is "A dog" smaller or larger than "A mouse"? Is 9999+8888 smaller or larger than 100*100? Replace the plus sign with a comparison operator in the following code to let Python check it for you and then answer. The result should return True if the correct comparison operator is used. 
+print("A dog" + "A mouse")
+print(9999+8888 + 100*100) 
+
+The solution below
+'''
+print("A dog" < "A mouse")
+print(9999+8888 > 100*100)
+
+
+# 5)
+'''
+    Question 5
+If a filesystem has a block size of 4096 bytes, this means that a file comprised of only one byte will still use 4096 bytes of storage. A file made up of 4097 bytes will use 4096*2=8192 bytes of storage. Knowing this, can you fill in the gaps in the calculate_storage function below, which calculates the total number of bytes needed to store a file of a given size?
+
+def calculate_storage(filesize):
+    block_size = 4096
+    # Use floor division to calculate how many blocks are fully occupied
+    full_blocks = ___
+    # Use the modulo operator to check whether there's any remainder
+    partial_block_remainder = ___
+    # Depending on whether there's a remainder or not, return
+    # the total number of bytes required to allocate enough blocks
+    # to store your data.
+    if partial_block_remainder > 0:
+        return ___
+    return ___
+
+print(calculate_storage(1))    # Should be 4096
+print(calculate_storage(4096)) # Should be 4096
+print(calculate_storage(4097)) # Should be 8192
+print(calculate_storage(6000)) # Should be 8192
+
+The solution below
+'''
+def calculate_storage(filesize):
+    block_size = 4096
+    # Use floor division to calculate how many blocks are fully occupied
+    full_blocks = filesize//block_size
+    # Use the modulo operator to check whether there's any remainder
+    partial_block_remainder = filesize % block_size
+    # Depending on whether there's a remainder or not, return
+    # the total number of bytes required to allocate enough blocks
+    # to store your data.
+    
+    if partial_block_remainder > 0:
+        return (full_blocks + 1) * block_size #Add 1 block for the partition block
+    return full_blocks * block_size
+
+print(calculate_storage(1))    # Should be 4096
+print(calculate_storage(4096)) # Should be 4096
+print(calculate_storage(4097)) # Should be 8192
+print(calculate_storage(6000)) # Should be 8192
