856f0bd02f1075cf0454f328601b3479042bd218
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-01-04 13:23:35 +0100

    Expressions and variables Exercises

---
+++ 01_Python_Crash_Course/02_Basic_Python_Sytax/2.type_conversion.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+# The arean of a triangle
+base = 6
+height = 3
+area = (base * height) / 2
+print("The area of the trangle is " + str(area))
+
+# Practice Quiz: Expressions and variables
+# 1. 
+'''
+Question 1
+This code is supposed to display "2 + 2 = 4" on the screen, but there is an error. Find the error in the code and fix it, so that the output is correct.
+
+The solution below
+'''
+print("2 + 2 = " + str(2 + 2))
+
+# 2.
+'''In this scenario, two friends are eating dinner at a restaurant. The bill comes in the amount of 47.28 dollars. The friends decide to split the bill evenly between them, after adding 15% tip for the service. Calculate the tip, the total amount to pay, and each friend's share, then output a message saying "Each person needs to pay: " followed by the resulting number.
+
+The solution below
+'''
+bill = 42.28 # Assign "bill" variable with bill amount
+tip = bill * 0.15 # Multiply by stated tip rate 
+total = bill + (bill * 0.15) # Sum the "total" of the "bill" and "tip"
+share = total / 2 # Divide "total" by number of friends dining
+print("Each person needs to pay " + str(share)) # Enter the required string and "share" 
+# Hint: Remember to convert incompatible data types
+
+
+# 3.
+'''
+Question 3
+This code is supposed to take two numbers, divide one by another so that the result is equal to 1, and display the result on the screen. Unfortunately, there is an error in the code. Find the error and fix it, so that the output is correct.
+numerator = 10
+denominator = 0
+result = numerator / denominator
+print(result)
+
+Solution below
+'''
+numerator = 10
+denominator = 10
+result = numerator / denominator
+print(result)
+
+# 4.
+'''
+Question 4
+Combine the variables to display the sentence "How do you like Python so far?"
+word1 = "How"
+word2 = "do"
+word3 = "you"
+word4 = "like"
+word5 = "Python"
+word6 = "so"
+word7 = "far?"
+
+print(___)
+
+Solution below
+'''
+word1 = "How"
+word2 = "do"
+word3 = "you"
+word4 = "like"
+word5 = "Python"
+word6 = "so"
+word7 = "far?"
+
+print(word1, word2, word3, word4, word5, word6, word7)
+
+# 5.
+'''
+What do you call a combination of numbers, symbols, or other values that produce a result when evaluated?
+
+Solution below
+'''
+# It is an expression
