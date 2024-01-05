84acb60bbf579b92a8cfdc1f0f71824bd9ccb243
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-01-05 18:19:11 +0100

    Recursion

---
+++ 01_Python_Crash_Course/03_loops/3.recursion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Recursion
+
+def factorial(n):
+    print("Factorial called with " +str(n))
+    if n < 2:
+        print("Returning 1")
+        return 1
+    result = n * factorial(n - 1)
+    print("Returning " + str(result) + " for factorial of " + str(n))
+    return result
+
+factorial(4)
