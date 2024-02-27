f15d34cccbea0f658ee8b4309836ce04dfba8be3
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-02-27 19:40:04 +0100

    lists and tuple

---
+++ 01_Python_Crash_Course/04_Strings_Lists_Dictionaries/2.lists_tuple.py
@@ -0,0 +1,46 @@
+# Iterating over lists and tupules
+'''
+animals = ["Lion", "Zebra", "Dolphin", "Monkey"]
+chars = 0
+for animal in animals:
+  chars += len(animal)
+
+print("Total characters: {}, Average length: {}".format(chars, chars/len(animals)))'''
+
+
+'''
+winners = ["Ashley", "Dylan", "Reese"]
+for index, person in enumerate(winners):
+  print("{} - {}".format(index + 1, person))
+'''
+
+'''
+def full_emails(people):
+  result = []
+  for email, name in people:
+    result.append("{} <{}>".format(name, email))
+  return result
+print(full_emails([("alex@example.com", "Alex Diego"), ("shay@example.com", "Shay Brandt")]))
+'''
+
+'''
+# For List Vs List Comprehension
+### Simple List Comprehension
+print("List comprehension result:")
+
+# The following list comprehension compacts several lines 
+# of code into one line:
+print([x*2 for x in range(1,11)])
+
+### Long form for loop
+print("Long form code result:")
+
+# The list comprehension above accomplishes the same result as
+# the long form version of the code shown below:
+my_list = []
+for x in range(1,11):
+    my_list.append(x*2)
+print(my_list)
+
+# Click Run to compare the two results.
+'''
